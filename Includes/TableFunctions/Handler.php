cc69d072794260ebaa18f2e73970b56be6e8e9a6
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-08-31 15:23:05 +0300

    search home page done

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -427,92 +427,6 @@ class Handler
     }
 
 
-    function searchFullText()
-    {
-        $search_algorithm = "fulltext";
-        // SELECT * FROM products WHERE MATCH (name) AGAINST ('cooking oil')
-
-        // create the base variables for building the search query
-        $search_string = "SELECT * FROM products WHERE published = 1 AND ";
-        $display_words = "";
-
-        // format each of search keywords into the db query to be run
-        $search_string .= "MATCH (name,tags) AGAINST ('" . $this->query . "' IN NATURAL LANGUAGE MODE)";
-        $display_words .= $this->query . ' ';
-
-//        echo $search_string;
-        // run the query in the db and search through each of the records returned
-        $query = mysqli_query($this->conn, $search_string);
-        $result_count = mysqli_num_rows($query);
-
-        $this->pageno = floatval($this->page);
-        $no_of_records_per_page = 10;
-        $offset = ($this->pageno - 1) * $no_of_records_per_page;
-
-
-        $total_rows = floatval(number_format($result_count));
-        $total_pages = ceil($total_rows / $no_of_records_per_page);
-
-
-        $itemRecords = array();
-
-
-        // check if the search query returned any results
-        if ($result_count > 0) {
-
-            $categoryids = array();
-            $menuCategory = array();
-
-
-            $category_stmt = $search_string . " LIMIT " . $offset . "," . $no_of_records_per_page . "";
-
-
-            $menu_type_id_result = mysqli_query($this->conn, $category_stmt);
-
-            while ($row = mysqli_fetch_array($menu_type_id_result)) {
-
-                array_push($categoryids, $row);
-            }
-
-            foreach ($categoryids as $row) {
-                $product = new Product($this->conn, intval($row['id']));
-                $temp = array();
-                $temp['id'] = $product->getId();
-                $temp['name'] = $product->getName();
-                $temp['category_id'] = $product->getCategory_id();
-                $temp['photos'] = $product->getPhotos();
-                $temp['thumbnail_img'] = $product->getThumbnail_img();
-                $temp['unit_price'] = intVal($product->getUnit_price());
-                $temp['discount'] = intVal($product->getDiscount());
-                $temp['purchase_price'] = intVal($product->getPurchase_price());
-                $temp['meta_title'] = $product->getMeta_title();
-                $temp['meta_description'] = $product->getMeta_description();
-                $temp['meta_img'] = $product->getMeta_img();
-                $temp['min_qtn'] = $product->getMin_qty();
-                $temp['published'] = $product->getPublished();
-                array_push($menuCategory, $temp);
-            }
-
-
-            $itemRecords["page"] = $this->pageno;
-            $itemRecords["searchTerm"] = $display_words;
-            $itemRecords["algorithm"] = $search_algorithm;
-            $itemRecords["products"] = $menuCategory;
-            $itemRecords["total_pages"] = $total_pages;
-            $itemRecords["total_results"] = $total_rows;
-
-        } else {
-            $itemRecords["page"] = $this->pageno;
-            $itemRecords["searchTerm"] = $display_words;
-            $itemRecords["algorithm"] = $search_algorithm;
-            $itemRecords["products"] = null;
-            $itemRecords["total_pages"] = $total_pages;
-            $itemRecords["total_results"] = $total_rows;
-        }
-
-        return $itemRecords;
-    }
-
     function readUserLikedSongs()
     {
         $itemRecords = array();
@@ -674,5 +588,220 @@ class Handler
         return $itemRecords;
     }
 
+    public function searchHomePage()
+    {
 
+        $home_page = (isset($_GET['page']) && $_GET['page']) ? htmlspecialchars(strip_tags($_GET["page"])) : '1';
+
+        $page = floatval($home_page);
+        $no_of_records_per_page = 12;
+        $offset = ($page - 1) * $no_of_records_per_page;
+
+        $sql = "SELECT COUNT(DISTINCT(genre)) as count FROM songs  WHERE tag != 'ad' ORDER BY `songs`.`weekplays` DESC limit 1";
+        $result = mysqli_query($this->conn, $sql);
+        $data = mysqli_fetch_assoc($result);
+        $total_rows = floatval($data['count']);
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+        $menuCategory = array();
+        $itemRecords = array();
+
+
+        if ($page == 1) {
+
+            // get_Slider_banner
+            $slider_id = array();
+            $sliders = array();
+
+
+            $slider_query = "SELECT id FROM playlist_sliders WHERE status=1 ORDER BY date_created DESC LIMIT 8";
+            $slider_query_id_result = mysqli_query($this->conn, $slider_query);
+            while ($row = mysqli_fetch_array($slider_query_id_result)) {
+                array_push($slider_id, $row['id']);
+            }
+
+
+            foreach ($slider_id as $row) {
+                $temp = array();
+                $slider = new PlaylistSlider($this->conn, $row);
+                $temp['id'] = $slider->getId();
+                $temp['playlistID'] = $slider->getPlaylistID();
+                $temp['imagepath'] = $slider->getImagepath();
+                array_push($sliders, $temp);
+            }
+
+            $slider_temps = array();
+            $slider_temps['heading'] = "Discover";
+            $slider_temps['search_sliders'] = $sliders;
+            array_push($menuCategory, $slider_temps);
+            // end get_Slider_banner
+
+
+            //get genres
+            //  popular search Begin
+            $bestsellingProductsID = array();
+            $bestSellingProducts = array();
+            $category_stmts = "SELECT `id`, `query`, `count`, `created_at`, `updated_at` FROM `searches` ORDER BY count DESC LIMIT 30";
+            $menu_type_id_results = mysqli_query($this->conn, $category_stmts);
+
+            while ($row = mysqli_fetch_array($menu_type_id_results)) {
+
+                array_push($bestsellingProductsID, $row);
+            }
+
+            foreach ($bestsellingProductsID as $row) {
+                $temp = array();
+                $temp['id'] = $row['id'];
+                $temp['query'] = $row['query'];
+                $temp['count'] = $row['count'];
+                $temp['created_at'] = $row['created_at'];
+                $temp['updated_at'] = $row['updated_at'];
+                array_push($bestSellingProducts, $temp);
+            }
+
+
+            $slider_temps = array();
+            $slider_temps['popularSearch'] = $bestSellingProducts;
+            array_push($menuCategory, $slider_temps);
+
+            // end popular search  Fetch
+
+            // end genres
+
+
+
+        }
+
+
+        //fetch other categories Begin
+        $home_genreIDs = array();
+        $genre_stmt = "SELECT DISTINCT(genre) FROM songs  WHERE tag != 'ad' ORDER BY `songs`.`plays` DESC LIMIT " . $offset . "," . $no_of_records_per_page . "";
+        $genre_stmt_result = mysqli_query($this->conn, $genre_stmt);
+
+        while ($row = mysqli_fetch_array($genre_stmt_result)) {
+
+            array_push($home_genreIDs, $row['genre']);
+        }
+
+        foreach ($home_genreIDs as $row) {
+            $genre = new Genre($this->conn, $row);
+            $temp = array();
+            $temp['id'] = $genre->getGenreid();
+            $temp['name'] = $genre->getGenre();
+            $temp['tag'] = $genre->getTag();
+            array_push($menuCategory, $temp);
+        }
+
+        $itemRecords["version"] = $this->version;
+        $itemRecords["page"] = $page;
+        $itemRecords["searchMain"] = $menuCategory;
+        $itemRecords["total_pages"] = $total_pages;
+        $itemRecords["total_results"] = $total_rows;
+
+        return $itemRecords;
+    }
+
+
+    function searchFullText()
+    {
+        $search_algorithm = "fulltext";
+        // SELECT * FROM products WHERE MATCH (name) AGAINST ('cooking oil')
+
+        // create the base variables for building the search query
+        $search_string = "SELECT * FROM products WHERE published = 1 AND ";
+        $display_words = "";
+
+        // format each of search keywords into the db query to be run
+        $search_string .= "MATCH (name,tags) AGAINST ('" . $this->query . "' IN NATURAL LANGUAGE MODE)";
+        $display_words .= $this->query . ' ';
+
+//        echo $search_string;
+        // run the query in the db and search through each of the records returned
+        $query = mysqli_query($this->conn, $search_string);
+        $result_count = mysqli_num_rows($query);
+
+        $this->pageno = floatval($this->page);
+        $no_of_records_per_page = 10;
+        $offset = ($this->pageno - 1) * $no_of_records_per_page;
+
+
+        $total_rows = floatval(number_format($result_count));
+        $total_pages = ceil($total_rows / $no_of_records_per_page);
+
+
+        $itemRecords = array();
+
+        $sch_term = htmlspecialchars(strip_tags($this->query));
+        $sh_result = mysqli_query($this->conn, "SELECT * FROM `searches` WHERE `query`='" . $sch_term . "' LIMIT 1;");
+        $sh_data = mysqli_fetch_assoc($sh_result);
+        if ($sh_data != null) {
+            $sh_id = floatval($sh_data['id']);
+            $countQuery = mysqli_query($this->conn,"SELECT `count` FROM searches WHERE id = $sh_id");
+            $shq_data = mysqli_fetch_assoc($countQuery);
+            $shq_count = floatval($shq_data['count']);
+            $shq_count += 1;
+            mysqli_query($this->conn, "UPDATE `searches` SET `count`= $shq_count WHERE id = $sh_id");
+
+        } else {
+            //insert data
+            mysqli_query($this->conn, "INSERT INTO `searches`(`query`, `count`) VALUES ('" . $sch_term . "',1)");
+        }
+
+
+
+        // check if the search query returned any results
+        if ($result_count > 0) {
+
+            $categoryids = array();
+            $menuCategory = array();
+
+
+            $category_stmt = $search_string . " LIMIT " . $offset . "," . $no_of_records_per_page . "";
+
+
+            $menu_type_id_result = mysqli_query($this->conn, $category_stmt);
+
+            while ($row = mysqli_fetch_array($menu_type_id_result)) {
+
+                array_push($categoryids, $row);
+            }
+
+            foreach ($categoryids as $row) {
+                $product = new Product($this->conn, intval($row['id']));
+                $temp = array();
+                $temp['id'] = $product->getId();
+                $temp['name'] = $product->getName();
+                $temp['category_id'] = $product->getCategory_id();
+                $temp['photos'] = $product->getPhotos();
+                $temp['thumbnail_img'] = $product->getThumbnail_img();
+                $temp['unit_price'] = intVal($product->getUnit_price());
+                $temp['discount'] = intVal($product->getDiscount());
+                $temp['purchase_price'] = intVal($product->getPurchase_price());
+                $temp['meta_title'] = $product->getMeta_title();
+                $temp['meta_description'] = $product->getMeta_description();
+                $temp['meta_img'] = $product->getMeta_img();
+                $temp['min_qtn'] = $product->getMin_qty();
+                $temp['published'] = $product->getPublished();
+                array_push($menuCategory, $temp);
+            }
+
+
+            $itemRecords["page"] = $this->pageno;
+            $itemRecords["searchTerm"] = $display_words;
+            $itemRecords["algorithm"] = $search_algorithm;
+            $itemRecords["products"] = $menuCategory;
+            $itemRecords["total_pages"] = $total_pages;
+            $itemRecords["total_results"] = $total_rows;
+
+        } else {
+            $itemRecords["page"] = $this->pageno;
+            $itemRecords["searchTerm"] = $display_words;
+            $itemRecords["algorithm"] = $search_algorithm;
+            $itemRecords["products"] = null;
+            $itemRecords["total_pages"] = $total_pages;
+            $itemRecords["total_results"] = $total_rows;
+        }
+
+        return $itemRecords;
+    }
 }
--- Requests/endpoints/searchhomepage.php
+++ Requests/endpoints/searchhomepage.php
@@ -1,28 +1,23 @@
 <?php
-// http://localhost/projects/KakebeAPI/Requests/category/sectionedCategory.php?page=2
-
-
+//header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 include_once 'includedFiles.php';
 
-
 if (!empty($db)) {
-
-    $cat_page = (isset($_GET['page']) && $_GET['page']) ? $_GET['page'] : '1';
-    $category = new HomeFeed($db, $cat_page);
-
-
+    $category = new Handler($db);
     $result = $category->searchHomePage();
 
-    if ($result) {
+
+    if($result){
         http_response_code(200);
         echo json_encode($result);
-    } else {
+    }else{
         http_response_code(404);
         echo json_encode(
             array("message" => "No item found.")
         );
     }
+
 }
 ?>
