5239f8430f0deb21fdca94140e6670b7fe76cb04
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-10-13 14:56:40 +0300

    update on user data done

--- Requests/endpoints/updateTrackUserData.php
+++ Requests/endpoints/updateTrackUserData.php
@@ -10,31 +10,26 @@ if (!empty($db)) {
 
     $data = json_decode(file_get_contents("php://input"));
 //
-    if(!empty($data->user_id) && !empty($data->liteRecentTrackList) && !empty($data->liteLikedTrackList)){
+    if (!empty($data->user_id) && !empty($data->liteRecentTrackList) && !empty($data->liteLikedTrackList)) {
         $handler = new Handler($db);
 
         $current_Time_InSeconds = time();
-        $update_date = date('Y-m-d H:i:s', $current_Time_InSeconds );
+        $update_date = date('Y-m-d H:i:s', $current_Time_InSeconds);
         $handler->user_id = $data->user_id;
         $handler->update_date = $update_date;
         $handler->liteRecentTrackList = $data->liteRecentTrackList;
         $handler->liteLikedTrackList = $data->liteLikedTrackList;
-        if($handler->updateTrackUserData()){
+        $result = $handler->updateTrackUserData();
+        if ($result) {
             http_response_code(200);
-            $response['error'] = false;
-            $response['message'] = 'Updated Successfully';
-            $response['trackIds'] = [];
-
-            echo json_encode($response);
-        }else{
+            echo json_encode($result);
+        } else {
             http_response_code(404);
             echo json_encode(
-                array("error" => true),
-                array("message" => "Update failed."),
-                array("trackIds" => []),
+                array("message" => "No item found.")
             );
         }
-    }else{
+    } else {
         http_response_code(400);
         $response['error'] = true;
         $response['message'] = 'Update failed. Data is incomplete.';
