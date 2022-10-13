aea5995fd625d07dc051bf3b018faeab5cc9ecbf
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-10-13 14:32:03 +0300

    update on user data done

--- Requests/endpoints/updateTrackUserData.php
+++ Requests/endpoints/updateTrackUserData.php
@@ -5,19 +5,38 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 include_once 'includedFiles.php';
 
+
 if (!empty($db)) {
-    $category = new Handler($db);
-    $result = $category->EventsHome();
+    echo "love";
 
-    if($result){
-        http_response_code(200);
-        echo json_encode($result);
+    $handler = new Handler($db);
+    $data = json_decode(file_get_contents("php://input"));
+
+    if(!empty($data->user_id) && !empty($data->liteRecentTrackList) && !empty($data->liteLikedTrackList)){
+
+        $current_Time_InSeconds = time();
+        $update_date = date('Y-m-d H:i:s', $current_Time_InSeconds );
+
+        $result = $handler->updateTrackUserData($data->user_id, $data->liteRecentTrackList, $data->liteLikedTrackList,$update_date);
+        if($result){
+            http_response_code(200);
+            echo json_encode($result);
+        }else{
+            http_response_code(404);
+            echo json_encode(
+                array("error" => true),
+                array("message" => "Update failed."),
+                array("trackIds" => []),
+            );
+        }
     }else{
-        http_response_code(404);
-        echo json_encode(
-            array("message" => "No item found.")
-        );
-    }
+        http_response_code(400);
+        $response['error'] = true;
+        $response['message'] = 'Update failed. Data is incomplete.';
+        $response['trackIds'] = [];
 
+        echo json_encode($response);
+    }
 }
+
 ?>
