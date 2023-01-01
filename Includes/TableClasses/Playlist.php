8d4e4a46eccec6e620a801a3021d1a630601448a
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-01-01 07:40:56 +0300

    update more

--- Includes/TableClasses/Playlist.php
+++ Includes/TableClasses/Playlist.php
@@ -123,15 +123,19 @@ class Playlist
         return mysqli_num_rows($query);
     }
 
-    public function getSongIds() {
+    public function getSongIds()
+    {
         $array = array();
         $query = "SELECT DISTINCT songId as id FROM playlistsongs WHERE playlistId=? ORDER BY playlistOrder ASC";
         $stmt = mysqli_prepare($this->con, $query);
         mysqli_stmt_bind_param($stmt, "s", $this->id);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        mysqli_stmt_close($stmt);
+        if ($stmt && mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_get_result($stmt);
+            $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            mysqli_stmt_close($stmt);
+        } else {
+            // handle the error
+        }
 
         switch ($this->id) {
             case "mwPL_query_base_rap_genre":
@@ -144,20 +148,20 @@ class Playlist
                 $query = "SELECT f.songid as id, s.title, s.genre, s.tag, g.name, SUM(f.playsmonth) as total_plays FROM frequency f JOIN songs s ON f.songid = s.id JOIN genres g on s.genre = g.id WHERE f.lastPlayed BETWEEN DATE_SUB(NOW(), INTERVAL 2 WEEK) AND NOW() AND s.tag = 'music' AND s.genre != 3 GROUP BY f.songid ORDER BY total_plays DESC, f.lastPlayed DESC LIMIT 20";
                 break;
             default:
-                break;
         }
-
         if (!empty($query)) {
             $stmt = mysqli_prepare($this->con, $query);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-            $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            mysqli_stmt_close($stmt);
+            if ($stmt && mysqli_stmt_execute($stmt)) {
+                $result = mysqli_stmt_get_result($stmt);
+                $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                mysqli_stmt_close($stmt);
+            } else {
+                // handle the error
+            }
         }
 
         return array_column($array, 'id');
     }
-
 
 
 }
