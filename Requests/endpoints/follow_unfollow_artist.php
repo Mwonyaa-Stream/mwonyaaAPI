fe309679db6fee6db1f409b22b6ceaa9d1b1daf1
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-01-16 22:17:47 +0300

    fixed on follow

--- Requests/endpoints/follow_unfollow_artist.php
+++ Requests/endpoints/follow_unfollow_artist.php
@@ -1,20 +1,17 @@
 <?php
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-// Connect to the database
-try {
-//    $db = new PDO("mysql:host=localhost;dbname=mwonya", "root", "");
-    $db = new PDO("mysql:host=178.79.148.46;dbname=mwonya", "mobile", "PBWuyqRPtjfq13GB");
-} catch (PDOException $e) {
-    // Return error message if connection fails
-    echo json_encode(["status" => "error", "message" => "Error connecting to the database: " . $e->getMessage()]);
+include_once 'includedFiles.php';
+$conn = $db;
+$message = "success";
+
+if ($conn == null) {
+    echo json_encode(["status" => "error", "message" => "Error connecting to the database"]);
     exit;
 }
 
-// Get the user ID and artist ID from the request
-
-
-if (isTheseParametersAvailable(array('userId', 'artistId', 'action'))){
+if (isTheseParametersAvailable(array('userId', 'artistId', 'action'))) {
     $userId = $_POST['userId'];
     $artistId = $_POST['artistId'];
     $action = $_POST['action'];
@@ -25,36 +22,32 @@ if (isTheseParametersAvailable(array('userId', 'artistId', 'action'))){
         exit;
     }
 
-    if($action == 'follow') {
+    if ($action == 'follow') {
         // Check if record already exists
-        $stmt = $db->prepare("SELECT COUNT(*) FROM artistfollowing WHERE artistid = ? AND userid = ?");
-        $stmt->execute([$artistId, $userId]);
-        $count = $stmt->fetchColumn();
-        if($count > 0) {
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM artistfollowing WHERE artistid = ? AND userid = ?");
+        $stmt->bind_param("ss", $artistId, $userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        $count = $result->fetch_row()[0];
+        if ($count > 0) {
             echo json_encode(["status" => "error", "message" => "Artist already followed by user"]);
             exit;
         } else {
             // Insert a new row into the artistfollowing table
-            try {
-                $stmt = $db->prepare("INSERT INTO artistfollowing (artistid, userid, datefollowed) VALUES (?, ?, NOW())");
-                $stmt->execute([$artistId, $userId]);
-            } catch (PDOException $e) {
-                // Return error message if insert fails
-                echo json_encode(["status" => "error", "message" => "Error inserting data into the database: " . $e->getMessage()]);
-                exit;
-            }
+            $stmt = $conn->prepare("INSERT INTO artistfollowing (artistid, userid, datefollowed) VALUES (?, ?, NOW())");
+            $stmt->bind_param("ss", $artistId, $userId);
+            $stmt->execute();
+            $message = "Now following Artist";
         }
-    } elseif($action == 'unfollow') {
-        // Delete the corresponding row from the artistfollowing table
-        try {
-            $stmt = $db->prepare("DELETE FROM artistfollowing WHERE artistid = ? AND userid = ?");
-            $stmt->execute([$artistId, $userId]);
-        } catch (PDOException $e) {
-            // Return error message if delete fails
-            echo json_encode(["status" => "error", "message" => "Error deleting data from the database: " . $e->getMessage()]);
-            exit;
-        }
-    }else{
+    } elseif ($action == 'unfollow') {
+            // Delete the corresponding row from the artistfollowing table
+        $stmt = $conn->prepare("DELETE FROM artistfollowing WHERE artistid = ? AND userid = ?");
+        $stmt->bind_param("ss", $artistId, $userId);
+        $stmt->execute();
+        $message = "Artist Unfollowed";
+
+    } else {
         echo json_encode(["status" => "error", "message" => "Invalid Action. Only 'follow' or 'unfollow' actions are allowed"]);
         exit;
     }
@@ -63,22 +56,19 @@ if (isTheseParametersAvailable(array('userId', 'artistId', 'action'))){
     exit;
 }
 
-
 // Return success message
-echo json_encode(["status" => "success"]);
+echo json_encode(["status" => "success", "message" => $message]);
 
 function isTheseParametersAvailable($params)
-{
-
-    //traversing through all the parameters
+{//traversing through all the parameters
     foreach ($params as $param) {
-        //if the paramter is not available
+//if the paramter is not available
         if (!isset($_POST[$param])) {
-            //return false
+//return false
             return false;
         }
     }
-    //return true if every param is available
+//return true if every param is available
     return true;
 }
 
