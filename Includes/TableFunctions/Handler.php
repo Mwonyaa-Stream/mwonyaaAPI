ca0a089792a3ad8037ae9e6f08e291099e022ff5
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-06-21 21:45:38 +0300

    adjust weekly

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -301,6 +301,52 @@ class Handler
             array_push($menuCategory, $slider_temps);
             // end get_Slider_banner
 
+            // weekly Now
+            $featured_weekly = array();
+            $tracks_weekly= array();
+            $weekly_now_sql = "SELECT songid as song_id, COUNT(*) AS play_count FROM frequency WHERE lastPlayed BETWEEN CURDATE() - INTERVAL 7 DAY AND CURDATE() GROUP BY songid ORDER BY play_count DESC LIMIT 10";
+            // Set up the prepared statement
+            $stmt = mysqli_prepare($this->conn, $weekly_now_sql);
+            // Execute the query
+            mysqli_stmt_execute($stmt);
+            // Bind the result variables
+            mysqli_stmt_bind_result($stmt, $song_id, $play_count);
+            // Fetch the results
+            while (mysqli_stmt_fetch($stmt)) {
+                array_push($featured_weekly, $song_id);
+            }
+            mysqli_stmt_close($stmt);
+
+            foreach ($featured_weekly as $track) {
+                $song = new Song($this->conn,$track);
+                $temp = array();
+                $temp['id'] = $song->getId();
+                $temp['title'] = $song->getTitle();
+                $temp['artist'] = $song->getArtist()->getName();
+                $temp['artistID'] = $song->getArtistId();
+                $temp['album'] = $song->getAlbum()->getTitle();
+                $temp['artworkPath'] = $song->getAlbum()->getArtworkPath();
+                $temp['genre'] = $song->getGenre()->getGenre();
+                $temp['genreID'] = $song->getGenre()->getGenreid();
+                $temp['duration'] = $song->getDuration();
+                $temp['lyrics'] = $song->getLyrics();
+                $temp['path'] = $song->getPath();
+                $temp['totalplays'] = $song->getPlays();
+                $temp['weeklyplays'] = $song->getWeeklyplays();
+                array_push($tracks_weekly, $temp);
+
+            }
+
+            // Close the prepared statement
+            $feat_weekly = array();
+            $feat_weekly['heading'] = "# Weekly Top 10";
+            $feat_weekly['subheading'] = "Featuring all the major chart hits";
+            $feat_weekly['type'] = "timely";
+            $feat_weekly['Tracks'] = $tracks_weekly;
+            array_push($menuCategory, $feat_weekly);
+
+            // end weekly
+
 
             // recently played array
             $recently_played = array();
