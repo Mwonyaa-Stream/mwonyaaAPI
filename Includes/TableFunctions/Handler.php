404969794f0c4916efcc573d5355625cd77a2098
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-10-22 12:12:50 +0300

    fixed on notifcation date

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -1356,8 +1356,7 @@ class Handler
                     $temp['plays'] = $row['plays'];
                     $temp['weekplays'] = $row['weekplays'];
                     $temp['artworkPath'] = $song->getAlbum()->getArtworkPath();
-//                    $temp['description'] = $song->getArtist()->getName() .$song->getFeaturing() . " added a new " . $name . " '" . $row['title'] . "'. give it a listen!";
-                    $temp['description'] = "New " . $name . " alert! '" . $row['title'] . "' by " . $song->getArtist()->getName() . $song->getFeaturing() . " is now playing on Mwonya. Tap to listen!";
+                    $temp['description'] = "New " . $name . " on Mwonya. " . $song->getArtist()->getName() . $song->getFeaturing() . "";
                     $temp['type'] = $row['type'];
                     $temp['tag'] = $row['tag'];
                     $temp['date'] = $row['dateAdded'];
