3191776d49ef66ad5d697f4389a31ce386c40751
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-11-07 00:29:12 +0300

    update on share track button

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -573,7 +573,7 @@ class Handler
                 $temp['heading'] = "New Release From";
                 $temp['title'] = $al->getTitle();
                 $temp['artworkPath'] = $al->getArtworkPath();
-                $temp['tag'] = $al->getDatecreated() . ' - ' . $al->getTag();
+                $temp['tag'] = $al->getReleaseDate() . ' - ' . $al->getTag();
                 $temp['artistId'] = $al->getArtistId();
                 $temp['artist'] = $al->getArtist()->getName();
                 $temp['artistArtwork'] = $al->getArtist()->getProfilePath();
@@ -786,7 +786,7 @@ class Handler
                 $temp['heading'] = "New Release For You";
                 $temp['title'] = $al->getTitle();
                 $temp['artworkPath'] = $al->getArtworkPath();
-                $temp['tag'] = $al->getDatecreated() . ' - ' . ucwords($al->getTag());
+                $temp['tag'] = $al->getReleaseDate() . ' - ' . ucwords($al->getTag());
                 $temp['artistId'] = $al->getArtistId();
                 $temp['artist'] = $al->getArtist()->getName();
                 $temp['artistArtwork'] = $al->getArtist()->getProfilePath();
