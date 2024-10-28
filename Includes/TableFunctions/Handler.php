72f8bd5c17c7500d323f037ff050d8cef0747271
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2024-10-29 02:35:45 +0300

    update on subscription filter

--- Includes/TableFunctions/Handler.php
+++ Includes/TableFunctions/Handler.php
@@ -4187,7 +4187,9 @@ class Handler
     public function Versioning()
     {
         $userID = isset($_GET['userID']) ? htmlspecialchars(strip_tags($_GET["userID"])) : 'general_user';
-        $user_subscription_sql = "SELECT `user_id` AS userId, `subscription_type` AS subscription_type, `amount` AS planCost, `plan_duration` AS durationInDays, UNIX_TIMESTAMP(`payment_created_date`) * 1000 AS date FROM `pesapal_transactions` WHERE user_id = '$userID' ORDER BY `payment_created_date` DESC LIMIT 1";
+        $current_now = date('Y-m-d H:i:s');
+        $user_subscription_sql = "SELECT pt.user_id AS userId, pt.subscription_type AS subscription_type, pt.amount AS planCost, pt.plan_duration AS durationInDays, UNIX_TIMESTAMP(pt.payment_created_date) * 1000 AS date FROM pesapal_transactions pt JOIN pesapal_payment_status pps ON pt.merchant_reference = pps.merchant_reference WHERE pt.user_id = '$userID' AND pps.status_code = 1 AND pt.plan_start_datetime <= '$current_now' AND pt.plan_end_datetime >= '$current_now' ORDER BY pt.payment_created_date DESC LIMIT 1";
+
         $subscription_details = array();
         $user_subscription_sql_result = mysqli_query($this->conn, $user_subscription_sql);
         while ($row = mysqli_fetch_array($user_subscription_sql_result)) {
